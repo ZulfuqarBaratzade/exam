69b1c0f936ba63e24de390e6fcd5cfaf49732b6b
zulfuqarbaratzade <zulfuqar.baratzade@gmail.com>
2023-07-30 03:03:39 +0400

    created login model

--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+@admin.register(Login_User)
+class LoginUserAdmin(admin.ModelAdmin):
+    list_display = ['id','username','password','profile_img','updated_date','created_date']
+    search_fields = ['id','username']
+    list_editable=['username','profile_img']
+
+    class Meta:
+        model = Login_User
--- login/views.py
+++ login/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .models import Login_User
 def login(request):
-    return render(request,"login.html")
+    if request.method == 'POST':
+        username = request.POST.get('Username')
+        password = request.POST.get('Password')
+        login_user = Login_User()
+        if login_user.username == username and login_user.password == password:
+            return redirect('index')
+        else:
+            # Handle invalid login credentials here (e.g., show an error message)
+            pass
+
+    return render(request, 'login.html')
